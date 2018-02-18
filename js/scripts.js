3a81c1cbb668dd1177cf6e66e65ac17178c0d3c1
Krzysztof Gertig <ktgertig@gmail.com>
2018-02-18 10:04:06 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // Eventy
 
-function checkOnclickEvent(param) {
-  console.log(param);
-}
-
 var addElem = document.getElementById('addElem'),
 	list = document.getElementById('list');
 
+addElem.addEventListener('click', function(e) {
+	var element = document.createElement('li');
+	element.innerHTML = 'item ' + document.getElementsByTagName('li').length;
+	list.appendChild(element);
+});
